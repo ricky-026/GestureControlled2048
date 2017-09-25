b30a43c72f9d7c443c6a5c435666a69560553f46
ricky-026 <rickyyuehao@gmail.com>
2017-09-24 23:52:33 -0400

    upload project

---
+++ app/src/main/java/lab0_203_28/uwaterloo/ca/lab0_203_28/Lab0_203_28.java
@@ -0,0 +1,13 @@
+package lab0_203_28.uwaterloo.ca.lab0_203_28;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab0_203_28 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab0_203_28);
+    }
+}
